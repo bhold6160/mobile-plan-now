1b84c56ee2ade68024fbb49c919bb0e6f2f97467
Kenny Hong <kennyhong@outlook.com>
2017-03-09 23:01:28 -0800

    added button

--- app.js
+++ app.js
@@ -6,10 +6,10 @@
 // }
 
 // Create prototype for hourly sale
-var planOne = ['$60','$70','$80','$90'];
-var planTwo = ['$160','$170','$180','$190'];
-var planThree = ['$260','$270','$280','$290'];
-var planFour = ['$360','$470','$580','$960'];
+// var planOne = ['$60','$70','$80','$90'];
+// var planTwo = ['$160','$170','$180','$190'];
+// var planThree = ['$260','$270','$280','$290'];
+// var planFour = ['$360','$470','$580','$960'];
 
 var oneLine = new mobilePlan('1 line',['$60','$70','$80','$90']);
 var twoLine = new mobilePlan('2 lines',['$160','$170','$180','$190']);
@@ -17,6 +17,8 @@ var threeLine = new mobilePlan('3 lines',['$260','$270','$280','$290']);
 var fourLine = new mobilePlan('4 lines',['$360','$470','$580','$960']);
 
 var table = document.getElementById('planTable');
+var resultsEl = document.getElementById('results');
+// var showResultsEl: document.getElementById('show-results');
 
 function mobilePlan(name, plan) {
   this.name = name;
