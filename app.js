7c1aa8186c8bcfe14a5c20f45dbd091a21990a98
Kenny Hong <kennyhong@outlook.com>
2017-03-09 22:55:06 -0800

    create list of price by number of lines

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+// var tracker = {
+//   attEl: document.getElementById('att'),
+//   verizonEl: document.getElementById('verizon'),
+//   tMobileEl: document.getElementById('t-mobile'),
+//   sprintEl: document.getElementById('sprint'),
+// }
+
+// Create prototype for hourly sale
+var planOne = ['$60','$70','$80','$90'];
+var planTwo = ['$160','$170','$180','$190'];
+var planThree = ['$260','$270','$280','$290'];
+var planFour = ['$360','$470','$580','$960'];
+
+var oneLine = new mobilePlan('1 line',['$60','$70','$80','$90']);
+var twoLine = new mobilePlan('2 lines',['$160','$170','$180','$190']);
+var threeLine = new mobilePlan('3 lines',['$260','$270','$280','$290']);
+var fourLine = new mobilePlan('4 lines',['$360','$470','$580','$960']);
+
+var table = document.getElementById('planTable');
+
+function mobilePlan(name, plan) {
+  this.name = name;
+  this.plan = plan;
+};
+
+mobilePlan.prototype.renderAsRow = function() {
+  var planTable = document.getElementById('planTable');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+  planTable.appendChild(trEl);
+
+  for (var i = 0; i < 4; i++)	{
+    // Get an element from li
+    var planPrice = document.createElement('td');
+    // Create and populate an LI element for each of the data points that you want to render
+    planPrice.textContent = this.plan[i];
+    // Append the li to ul
+    trEl.appendChild(planPrice);
+    planTable.appendChild(trEl);
+  }
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
+oneLine.renderAsRow();
+twoLine.renderAsRow();
+threeLine.renderAsRow();
+fourLine.renderAsRow();
