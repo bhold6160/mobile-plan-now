3f8891c5c90e402ff96b48940c32680ae319ec2f
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-03-11 15:40:43 -0800

    changed event listener and got rid of results button

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+var oneLine = new MobilePlan('1 line',['$60','$70','$80','$90']);
+var twoLine = new MobilePlan('2 lines',['$160','$170','$180','$190']);
+var threeLine = new MobilePlan('3 lines',['$260','$270','$280','$290']);
+var fourLine = new MobilePlan('4 lines',['$360','$470','$580','$960']);
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+var table = document.getElementById('planTable');
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+
+function MobilePlan(name, plan) {
+  this.name = name;
+  this.plan = plan;
+};
+
+MobilePlan.prototype.renderAsRow = function() {
+  var planTable = document.getElementById('planTable');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+  planTable.appendChild(trEl);
+
+  for (var i = 0; i < 4; i++)	{
+    // Get an element from li
+    var planPrice = document.createElement('td');
+    // Create and populate an LI element for each of the data points that you want to render
+    planPrice.textContent = this.plan[i];
+    // Append the li to ul
+    trEl.appendChild(planPrice);
+    planTable.appendChild(trEl);
   }
 };
+
+
+var tracker = {
+  selectEl: document.getElementById('select'),
+
+  checkClicks: function() {
+    this.selectEl.addEventListener('change', function(e){
+      // e.preventDefault();
+      var liEl = document.createElement('li');
+      if (document.getElementById('select').value == 'choose') {
+      } else if (document.getElementById('select').value == 'oneLine') {
+        console.log('oneLine');
+        liEl.textContent = oneLine.renderAsRow();
+      }  else if (document.getElementById('select').value == 'twoLine') {
+        console.log('twoLine');
+        liEl.textContent = twoLine.renderAsRow();
+      } else if (document.getElementById('select').value == 'threeLine') {
+        console.log('threeLine');
+        liEl.textContent = threeLine.renderAsRow();
+      } else if (document.getElementById('select').value == 'fourLine') {
+        console.log('fourLine');
+        liEl.textContent = fourLine.renderAsRow();
+      }
+    });
+  },
+};
+tracker.checkClicks();
