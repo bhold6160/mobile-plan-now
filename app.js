0cd42abe57da526c761eebcf64779ccc03bc5b1d
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-03-18 10:42:02 -0700

    combined javascript files and added login

--- app.js
+++ app.js
@@ -1,5 +1,84 @@
 // 'use strict'
 
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    localStorage.setItem('stringifiedUserInput', JSON.stringify(username, password));
+
+    if (username == "Formget" && password == "formget#123") {
+        alert("Login successfully");
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    } else {
+        attempt--; // Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+var verizonLine = [80, 140, 160, 180];
+var attLine = [60, 115, 135, 155];
+var tmobLine = [70, 100, 140, 160];
+var sprintLine = [50, 90, 90, 90];
+var numberOfLines = ['1 Line', '2 Lines', '3 Lines', '4 Lines'];
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chartData = {
+  type: 'bar',
+  data: {
+    labels: numberOfLines,
+    datasets: [
+      {
+        label: 'AT&T',
+        data: attLine,
+        // backgroundColor: 'opaque',
+        borderColor: '#098ad8',
+        borderWidth: 2,
+      },
+      {
+        label: 'Verizon',
+        data: verizonLine,
+        // backgroundColor: 'red',
+        borderColor: '#c30014',
+        borderWidth: 2,
+      },
+      {
+        label: 'T-Mobile',
+        data: tmobLine,
+        // backgroundColor: 'magenta',
+        borderColor: 'magenta',
+        borderWidth: 2,
+      },
+      {
+        label: 'Sprint',
+        data: sprintLine,
+        // backgroundColor: 'yellow',
+        borderColor: 'yellow',
+        borderWidth: 2,
+      },
+      {
+        label: 'Your Plan',
+        data: userInput[i],
+        // backgroundColor: 'yellow',
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+    ],
+  },
+};
+
+var myChart = new Chart(ctx, chartData);
+localStorage.getItem('stringifiedUserInput');
+
 (function () {
   var stringifiedAll = localStorage.getItem('stringifiedUserInput');
   var parsedAllProducts = JSON.parse(stringifiedAll);
