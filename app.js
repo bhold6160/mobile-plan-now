09e14a08d037b89ec19ba1a8914d7277c244636c
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-03-18 11:19:58 -0700

    login form working correctly

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 // 'use strict'
 
+(function () {
+  var stringifiedAll = localStorage.getItem('stringifiedUserInput');
+  var parsedAllProducts = JSON.parse(stringifiedAll);
+  for (var i in parsedAllProducts) {
+    userInput[i] = parsedAllProducts[i];
+  }
+});
+
+(function () {
+  var userNamePassword = localStorage.getItem('stringifiedNamePass');
+  var parsedNamePassword = JSON.parse(stringifiedInfo);
+  for (var i in parsedNamePassword) {
+    username = parsedNamePassword[i];
+    password = parsedNamePassword[i];
+  }
+});
+
+usernameArr = [];
+passwordArr = [];
+
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate() {
     var username = document.getElementById("username").value;
+    usernameArr.push(username)
     var password = document.getElementById("password").value;
-    localStorage.setItem('stringifiedUserInput', JSON.stringify(username, password));
+    passwordArr.push(password)
+    localStorage.setItem('stringifiedNamePass', JSON.stringify(username, password));
 
-    if (username == "Formget" && password == "formget#123") {
+    if (username == usernameArr && password == passwordArr) {
         alert("Login successfully");
-        window.location = "success.html"; // Redirecting to other page.
+        window.location = "index.html"; // Redirecting to other page.
         return false;
     } else {
         attempt--; // Decrementing by one.
@@ -23,70 +45,6 @@ function validate() {
         }
     }
 }
-
-var verizonLine = [80, 140, 160, 180];
-var attLine = [60, 115, 135, 155];
-var tmobLine = [70, 100, 140, 160];
-var sprintLine = [50, 90, 90, 90];
-var numberOfLines = ['1 Line', '2 Lines', '3 Lines', '4 Lines'];
-
-var ctx = document.getElementById('myChart').getContext('2d');
-
-var chartData = {
-  type: 'bar',
-  data: {
-    labels: numberOfLines,
-    datasets: [
-      {
-        label: 'AT&T',
-        data: attLine,
-        // backgroundColor: 'opaque',
-        borderColor: '#098ad8',
-        borderWidth: 2,
-      },
-      {
-        label: 'Verizon',
-        data: verizonLine,
-        // backgroundColor: 'red',
-        borderColor: '#c30014',
-        borderWidth: 2,
-      },
-      {
-        label: 'T-Mobile',
-        data: tmobLine,
-        // backgroundColor: 'magenta',
-        borderColor: 'magenta',
-        borderWidth: 2,
-      },
-      {
-        label: 'Sprint',
-        data: sprintLine,
-        // backgroundColor: 'yellow',
-        borderColor: 'yellow',
-        borderWidth: 2,
-      },
-      {
-        label: 'Your Plan',
-        data: userInput[i],
-        // backgroundColor: 'yellow',
-        borderColor: 'black',
-        borderWidth: 2,
-      },
-    ],
-  },
-};
-
-var myChart = new Chart(ctx, chartData);
-localStorage.getItem('stringifiedUserInput');
-
-(function () {
-  var stringifiedAll = localStorage.getItem('stringifiedUserInput');
-  var parsedAllProducts = JSON.parse(stringifiedAll);
-  for (var i in parsedAllProducts) {
-    userInput[i] = parsedAllProducts[i];
-  }
-});
-
 // var arrayNumber = [];
 
 // create table
@@ -260,5 +218,7 @@ var comparePlan = {
     });
   },
 };
+
 comparePlan.checkClicks();
+localStorage.getItem('stringifiedNamePass');
 localStorage.getItem('stringifiedUserInput');
